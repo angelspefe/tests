5392395616ac6ede50c8b9c94e78e3cac31ea001
Angels <angelspefe@hotmail.com>
2018-08-05 20:40:17 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // - - - - - - - - - OWL CAROUSEL
 
-
 $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
     loop:true,
@@ -25,52 +24,46 @@ $(document).ready(function(){
         }
     }
   })
-  });
-
+});
 
 
 
 // - - - - - - - - - MAGNIFIC GALLERY
 
 // $(document).ready(function() {
-// 	$('.gallery-popup').magnificPopup({
+// 	$('.galleryItem').magnificPopup({
+// 		type: 'image',
+// 		})
+// });
+
+// $(document).ready(function() {
+// 	$('.galleryItem').magnificPopup({
 // 		delegate: 'a',
 // 		type: 'image',
 // 		tLoading: 'Loading image #%curr%...',
-//     closeOnContentClick: true,
 // 		mainClass: 'mfp-img-mobile',
 // 		gallery: {
 // 			enabled: true,
 // 			navigateByImgClick: true,
 // 			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-//       tPrev: 'Previous (Left arrow key)', // title for left button
-//       tNext: 'Next (Right arrow key)', // title for right button
 // 		},
 // 		image: {
 // 			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
 // 			titleSrc: function(item) {
-// 				return item.el.attr('title') + '<small>Casa Mathilda</small>';
+// 				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
 // 			}
-// 		},
-// 	});
+// 		}
+// 	})
 // });
 
 $(document).ready(function() {
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-			}
-		}
-	});
+$('.popup-gallery').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+        delegate: 'a', // the selector for gallery item
+        type: 'image',
+        gallery: {
+          enabled:true
+        }
+    });
+});
 });
