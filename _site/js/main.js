986d3b6eda39ad924f5d76aeba7838aae7d855de
Angels <angelspefe@hotmail.com>
2018-08-08 09:50:00 +0200

    tests

--- _site/js/main.js
+++ _site/js/main.js
@@ -30,40 +30,20 @@ $(document).ready(function(){
 
 // - - - - - - - - - MAGNIFIC GALLERY
 
-// $(document).ready(function() {
-// 	$('.galleryItem').magnificPopup({
-// 		type: 'image',
-// 		})
-// });
-
-// $(document).ready(function() {
-// 	$('.galleryItem').magnificPopup({
-// 		delegate: 'a',
-// 		type: 'image',
-// 		tLoading: 'Loading image #%curr%...',
-// 		mainClass: 'mfp-img-mobile',
-// 		gallery: {
-// 			enabled: true,
-// 			navigateByImgClick: true,
-// 			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-// 		},
-// 		image: {
-// 			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-// 			titleSrc: function(item) {
-// 				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-// 			}
-// 		}
-// 	})
-// });
-
 $(document).ready(function() {
-$('.popup-gallery').each(function() { // the containers for all your galleries
-    $(this).magnificPopup({
-        delegate: 'a', // the selector for gallery item
-        type: 'image',
-        gallery: {
-          enabled:true
-        }
-    });
-});
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+      arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+		}
+	});
 });
